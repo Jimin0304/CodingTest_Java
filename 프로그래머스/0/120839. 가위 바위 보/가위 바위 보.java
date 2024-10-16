e8ff44d94364a8c4fb03323c4a0f526785b87db2
Jimin0304 <74671573+Jimin0304@users.noreply.github.com>
2024-10-16 17:49:13 +0900

    [level 0] Title: 가위 바위 보, Time: 0.08 ms, Memory: 70.5 MB -BaekjoonHub

--- 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.java
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.java
@@ -3,12 +3,17 @@ class Solution {
         StringBuilder answer = new StringBuilder();
         
         for (int i = 0; i < rsp.length(); i++) {
-            if (rsp.charAt(i) == '0')
-                answer.append('5');
-            else if (rsp.charAt(i) == '2')
-                answer.append('0');
-            else if (rsp.charAt(i) == '5')
-                answer.append('2');
+            switch(rsp.charAt(i)) {
+                case '0':
+                    answer.append('5');
+                    break ;
+                case '2':
+                    answer.append('0');
+                    break ;
+                case '5':
+                    answer.append('2');
+                    break ;
+            }
         }
         
         return answer.toString();
