16425940f55595ae1bfb9a6223a437b481281fff
Jimin0304 <74671573+Jimin0304@users.noreply.github.com>
2024-11-08 17:08:17 +0900

    [level 3] Title: 단속카메라, Time: 8.64 ms, Memory: 55.2 MB -BaekjoonHub

---
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[][] routes) {
+        int answer = 1;
+        Arrays.sort(routes, (o1, o2) -> {
+           return o1[1] - o2[1]; 
+        });
+        
+        int now = routes[0][1];
+        for (int i = 1; i < routes.length; i++) {
+            if (now >= routes[i][0] && now <= routes[i][1])
+                continue ;
+            now = routes[i][1];
+            answer++;
+        }
+        
+        return answer;
+    }
+}
