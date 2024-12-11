f6fde77466839ad89805ff43194c6171fd2488ba
Jimin0304 <74671573+Jimin0304@users.noreply.github.com>
2024-12-11 22:56:57 +0900

    [level 2] Title: 피로도, Time: 3.47 ms, Memory: 72.7 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,48 +1,22 @@
 import java.util.*;
-import java.util.stream.*;
-class Solution {    // 다시 풀기
+class Solution {
 
-    class State{
-        public int k;
-        public int cnt;
-        public boolean[] visited;
-        State(int k,int cnt,boolean[] visited){
-             this.k=k;
-             this.cnt=cnt;
-             this.visited=visited;
-        }
-
-    }
-
+    static int answer = 0;
+    
     public int solution(int k, int[][] dungeons) {
-        int answer = -1;
-
-        Queue<State> que=new LinkedList<>();
-        boolean[] visited=new boolean[dungeons.length];
-        //k 피로도 cnt 클리어던전개수, visited
-        int cnt=0;
-        que.add(new State(k,cnt,visited));
-
-        while(!que.isEmpty()){
-
-            State st= que.poll();
-            answer=Math.max(answer,st.cnt);
-
-
-            for(int i=0;i<dungeons.length;i++){
-                boolean[] tempV=Arrays.copyOf(st.visited,dungeons.length);
-                if(tempV[i]!=true && st.k>=dungeons[i][0]){
-                    tempV[i]=true;
-
-                    que.add(new State(st.k-dungeons[i][1],st.cnt+1,tempV.clone()));
-
-
-
-                }
+        boolean[] visited = new boolean[dungeons.length];
+        dfs(dungeons, visited, 0, k);
+        return answer;
+    }
+    
+    public void dfs(int[][] dungeons, boolean[] visited, int cnt, int k) {
+        answer = Math.max(answer, cnt);
+        for (int i = 0; i < dungeons.length; i++) {
+            if (!visited[i] && k >= dungeons[i][0]) {
+                visited[i] = true;
+                dfs(dungeons, visited, cnt + 1, k - dungeons[i][1]);
+                visited[i] = false;
             }
         }
-
-
-        return answer;
     }
 }   
