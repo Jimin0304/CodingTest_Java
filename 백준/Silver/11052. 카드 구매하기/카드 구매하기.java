69b974c487966302ab7a4000433aef2b4b4487f2
Jimin0304 <74671573+Jimin0304@users.noreply.github.com>
2025-03-03 21:08:34 +0900

    [Silver I] Title: 카드 구매하기, Time: 252 ms, Memory: 21032 KB -BaekjoonHub

---
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] P = new int[N + 1];
+        int[] D = new int[N + 1];
+        for (int i = 1; i <= N; i++) {    // init
+            P[i] = sc.nextInt();
+        }
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= i; j++) {
+                D[i] = Math.max(D[i], D[i - j] + P[j]);
+            }
+        }
+        
+        System.out.println(D[N]);
+    }
+}
